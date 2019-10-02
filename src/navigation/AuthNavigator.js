b7f49e505136b4cd506b3f2d3071f54d2967d28f
Felix Gerschau <felix.gerschau@gmail.com>
2019-10-02 12:29:14 +0200

    Add LoginScreen styles

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -5,16 +5,18 @@ import Colors from '../constants/Colors';
 import RecoverPasswordContainer from '../screens/Auth/RecoverPassword/RecoverPassword.container';
 
 export default createStackNavigator({
-  Login: LoginScreen,
+  Login: {
+    screen: LoginScreen,
+    navigationOptions: () => ({ header: null }),
+  },
   Signup: SignupScreen,
   RecoverPassword: RecoverPasswordContainer,
 }, {
   navigationOptions: {
     headerStyle: {
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grayBackground,
       elevation: 0,
       borderBottomWidth: 0,
-      
     },
     headerTintColor: '#000000',
   },
--- src/screens/NewEvent/NewEvent.component.js
+++ src/screens/NewEvent/NewEvent.component.js
@@ -162,7 +162,9 @@ const styles = {
     paddingTop: Layout.spacing * 3,
     backgroundColor: Colors.background,
     flex: 1,
+    position: 'absolute',
     paddingHorizontal: Layout.spacing,
+    top: (Platform.OS === 'ios' ? -90 : 0),
   },
   containerStyle: {
     position: 'absolute',
